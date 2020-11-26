bca1ddcd6a9bf8d997332942c9031e1a9807eee5
eric9178 <eric9178vadim@gmail.com>
2020-11-26 15:35:37 +0900

    fixed serveral issues

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -33,6 +33,7 @@ class Sales extends CI_Controller
         $sales = $this->JobModel->getSalesData();
         $profit_filter = $this->input->get('profit');
         $retAry = array();
+        $j = 0;
         for ($i = 0; $i < count($sales); $i++) {
             $total_pay_amount = $this->JobModel->getPayamountByJobID($sales[$i]['id']);
 
@@ -43,10 +44,12 @@ class Sales extends CI_Controller
                     continue;
                 }
             }
-            $retAry[$i] = $sales[$i];
-            $retAry[$i]['job_total'] = $sales[$i]['job_balance'];
-            $retAry[$i]['profit'] = $profit;
-            $retAry[$i]['job_balance'] = round($sales[$i]['job_balance'] * 1 - $total_pay_amount * 1, 2);
+
+            $retAry[$j] = $sales[$i];
+            $retAry[$j]['job_total'] = $sales[$i]['job_balance'];
+            $retAry[$j]['profit'] = $profit;
+            $retAry[$j]['job_balance'] = round($sales[$i]['job_balance'] * 1 - $total_pay_amount * 1, 2);
+            $j++;
         }
         $data['data'] = $retAry;
         echo json_encode($data);
